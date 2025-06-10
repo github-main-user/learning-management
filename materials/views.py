9e47056fdcffb856a97de72a22049d8c85de42e6
github-main-user <kolya.skripn@mail.ru>
2025-06-10 20:40:41 +0300

    feat: create views for the new models

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
 
-# Create your views here.
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+
+# COURSE
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+# LESSON
+
+
+class LessonListCreateView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
