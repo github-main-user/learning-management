775e6b88cf4356a83d1862df2fcb6947b9880385
github-main-user <kolya.skripn@mail.ru>
2025-06-10 20:18:38 +0300

    refactor: rename field and add __str__

--- materials/models.py
+++ materials/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     preview = models.ImageField(upload_to="courses/", blank=True, null=True)
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Lesson(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     preview = models.ImageField(upload_to="courses/", blank=True, null=True)
     video_url = models.URLField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons")
+
+    def __str__(self) -> str:
+        return self.title
