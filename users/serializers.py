4fb33f4880ada868e8d5aebc915e31469fa69651
github-main-user <kolya.skripn@mail.ru>
2025-06-13 00:03:58 +0300

    feat: add Payment serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,21 @@
 from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
 
+from .models import Payment
+
 User = get_user_model()
 
 
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ["id", "user", "timestamp", "course", "lesson", "amount", "method"]
+        read_only_fields = ["timestamp"]
+
+
 class ProfileSerializer(ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
         fields = ["id", "email", "first_name", "last_name", "phone", "city", "avatar"]
