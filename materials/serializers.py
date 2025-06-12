05face8e39aeca130736147d8e2f763b25deb0f3
github-main-user <kolya.skripn@mail.ru>
2025-06-12 23:21:47 +0300

    feat: lessons serializer now is nested (read only)

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,15 +1,21 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from .models import Course, Lesson
-
-
-class CourseSerializer(ModelSerializer):
-    class Meta:
-        model = Course
-        fields = "__all__"
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
-        fields = "__all__"
+        fields = ["id", "title", "description", "preview", "video_url", "course"]
+
+
+class CourseSerializer(ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+    lessons_count = SerializerMethodField()
+
+    def get_lessons_count(self, instance):
+        return instance.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ["id", "title", "description", "preview", "lessons", "lessons_count"]
