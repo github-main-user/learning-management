1789f5669011d7b9f24c86d8fa193fc82193b727
github-main-user <kolya.skripn@mail.ru>
2025-06-13 00:26:34 +0300

    feat: add filtering and ordering for payments

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
+from .models import Payment
 from .serializers import PaymentSerializer, ProfileSerializer
 
 User = get_user_model()
@@ -17,10 +20,14 @@ class ProfileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class PaymentListCreateView(generics.ListCreateAPIView):
-    queryset = User.objects.all()
+    queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    ordering_fields = ["timestamp"]
+    filterset_fields = ["course", "lesson", "method"]
 
 
 class PaymentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
+    queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
