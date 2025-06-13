5401d0094102bf8075c4c181db2e48a3943adc28
github-main-user <kolya.skripn@mail.ru>
2025-06-13 22:14:10 +0300

    chore: add namespaces

--- config/urls.py
+++ config/urls.py
@@ -2,14 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/materials/", include("materials.urls")),
-    path("api/users/", include("users.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/materials/", include("materials.urls", namespace="materials")),
+    path("api/users/", include("users.urls", namespace="users")),
 ]
 
 if settings.DEBUG:
