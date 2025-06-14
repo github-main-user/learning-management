07de7ca106eb5a9232146d29d2db03802571678a
github-main-user <kolya.skripn@mail.ru>
2025-06-14 16:20:02 +0300

    feat: add is_moderator permission

--- users/permissions.py
+++ users/permissions.py
@@ -17,3 +17,9 @@ class IsSelfOrReadOnly(permissions.BasePermission):
             return True
 
         return obj == request.user
+
+
+class IsModerator(permissions.BasePermission):
+    @override
+    def has_permission(self, request, view, obj):
+        return request.user.groups.filter(name="moderators").exists()
