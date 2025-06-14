708915cab57c67ddcc8d2eacd6d19a4f3f93d1bd
github-main-user <kolya.skripn@mail.ru>
2025-06-14 15:07:46 +0300

    feat: register user model in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "avatar",
+        "phone",
+        "city",
+        "is_active",
+    ]
+    search_fields = ["first_name", "last_name"]
+    list_filter = ["city", "is_active"]
