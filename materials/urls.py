ccdabfedb54fa0c38144073bfbaf2113d9502f9e
github-main-user <kolya.skripn@mail.ru>
2025-06-17 20:33:04 +0300

    refactor: update lesson list-create url name

--- materials/urls.py
+++ materials/urls.py
@@ -18,7 +18,7 @@ courses_router.register(r"courses", CourseViewAPISet, basename="course")
 urlpatterns = [
     *courses_router.urls,
     path("courses/", SubscriptionAPIView.as_view(), name="course-subscription"),
-    path("lessons/", LessonListCreateAPIView.as_view(), name="lessons-list-create"),
+    path("lessons/", LessonListCreateAPIView.as_view(), name="lesson-list"),
     path(
         "lessons/<int:pk>/",
         LessonRetrieveUpdateDestroyAPIView.as_view(),
