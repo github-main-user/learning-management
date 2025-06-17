7f615da3d3ccb1d977629ebd32c91c91b03c3297
github-main-user <kolya.skripn@mail.ru>
2025-06-17 20:11:31 +0300

    fix: add has_object_permission method for detial methods

--- users/permissions.py
+++ users/permissions.py
@@ -28,3 +28,7 @@ class IsModerator(permissions.BasePermission):
     @override
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moderators").exists()
+
+    @override
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
