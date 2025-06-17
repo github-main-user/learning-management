dd5e04b6c2987de64be285dd358c6ec5a12bcd87
github-main-user <kolya.skripn@mail.ru>
2025-06-18 00:33:44 +0300

    fix: add missing permission

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import Payment
 from .permissions import IsSelfOrReadOnly
@@ -33,7 +33,7 @@ class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
 
     queryset = User.objects.all()
-    permission_classes = [IsSelfOrReadOnly]
+    permission_classes = [IsAuthenticated, IsSelfOrReadOnly]
 
     @override
     def get_serializer_class(self):
