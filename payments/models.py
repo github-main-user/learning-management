a39141bb77f3f2133436158860acf22f7b0ec6cf
github-main-user <kolya.skripn@mail.ru>
2025-06-19 03:48:05 +0300

    feat: update payment model for stripe

--- payments/models.py
+++ payments/models.py
@@ -10,13 +10,17 @@ class Payment(models.Model):
     class PaymentMethod(models.TextChoices):
         CASH = "cash", "Cash"
         TRANSFER = "transfer", "Transfer"
+        STRIPE = "stripe", "Stripe"
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments")
     timestamp = models.DateTimeField(auto_now_add=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True)
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, blank=True, null=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, blank=True, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     method = models.CharField(max_length=8, choices=PaymentMethod)
+    stripe_session_id = models.CharField(max_length=255, blank=True, null=True)
+    payment_url = models.URLField(blank=True, null=True)
+    is_paid = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["timestamp"]
