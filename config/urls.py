a20af4b39b382fff4fb3c9a8e86b91ce00dbc3bc
github-main-user <kolya.skripn@mail.ru>
2025-06-19 19:05:57 +0300

    feat: add payments urls to main urls

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/materials/", include("materials.urls", namespace="materials")),
     path("api/users/", include("users.urls", namespace="users")),
+    path("api/payments/", include("payments.urls", namespace="payments")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(), name="redoc"),
--- payments/urls.py
+++ payments/urls.py
@@ -7,6 +7,6 @@ app_name = PaymentsConfig.name
 
 
 urlpatterns = [
-    path("payments/", PaymentListAPIView.as_view(), name="payment-list"),
-    path("payments/", PaymentCreateAPIView.as_view(), name="payment-create"),
+    path("", PaymentListAPIView.as_view(), name="payment-list"),
+    path("", PaymentCreateAPIView.as_view(), name="payment-create"),
 ]
