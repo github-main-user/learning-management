ae7894b7664e271f0f5fba2c0d20e0631d0d441f
github-main-user <kolya.skripn@mail.ru>
2025-06-21 20:32:27 +0300

    feat: add email backend setup

--- config/settings.py
+++ config/settings.py
@@ -109,10 +109,23 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "test@email.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.example.com")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+    EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() in _TRUE_VALUES
+    EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False").lower() in _TRUE_VALUES
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
 ]
-
 CORS_ALLOW_ALL_ORIGINS = False
 
 
