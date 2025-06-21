1cca210751ddb829b6ced3bf2247976786839237
github-main-user <kolya.skripn@mail.ru>
2025-06-21 21:32:32 +0300

    feat: add updated_at field for Course model

--- materials/models.py
+++ materials/models.py
@@ -10,6 +10,7 @@ class Course(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="courses"
     )
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.title
