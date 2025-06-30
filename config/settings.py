3ecd9ad6c33f12d1255699df9f885c7d0463aa95
github-main-user <kolya.skripn@mail.ru>
2025-06-30 17:11:12 +0300

    feat: setup nginx

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
